b55e37319e71fb06e914282ea3a26715f6542d7d
Lucas Rodriguez <110471982+lucasrdrz@users.noreply.github.com>
2026-04-19 21:47:37 -0300

    Update test.py

--- test.py
+++ test.py
@@ -268,38 +268,28 @@ st.markdown(
 
 st.markdown('<div class="divider"></div>', unsafe_allow_html=True)
 
-# --- REGALO ---
-st.markdown('<h2 class="fade-in">Regalo</h2>', unsafe_allow_html=True)
+st.markdown(f"""
+<div class="card fade-in">
 
-st.markdown("""
-<p class="center fade-in" style="font-size:24px;">
-Si querés hacernos un regalo 💕<br><br>
+<p style="text-align:center; font-size:24px;">
+Si querés hacernos un regalo <br><br>
 Podés ayudarnos con nuestra luna de miel ✈️
 </p>
-""", unsafe_allow_html=True)
 
-CBU = "0720176588000026340436"
-ALIAS = "CAMI.GABI.MACA"
-
-st.markdown(f'''
-<p class="center fade-in" style="font-size:22px;">
+<p style="text-align:center; font-size:22px;">
 <b>CBU:</b> {CBU}
 </p>
-''', unsafe_allow_html=True)
 
-st.markdown(f'''
-<p class="center fade-in" style="font-size:22px;">
+<p style="text-align:center; font-size:22px;">
 <b>Alias:</b> {ALIAS}
 </p>
-''', unsafe_allow_html=True)
 
-st.markdown(f'''
-<p class="center fade-in" style="font-size:18px; opacity:0.7;">
+<p style="text-align:center; font-size:18px; opacity:0.7;">
 👆 Tocá para copiar el CBU
 </p>
-''', unsafe_allow_html=True)
 
-st.markdown('<div class="divider"></div>', unsafe_allow_html=True)
+</div>
+""", unsafe_allow_html=True)
 
 # --- PLAYLIST ---
 st.markdown("""
