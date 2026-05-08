203cdfe890c899b0ff05f075cef7dfe2518e4357
Lucas Rodriguez <110471982+lucasrdrz@users.noreply.github.com>
2026-05-08 15:56:56 -0300

    Update test.py

--- test.py
+++ test.py
@@ -258,7 +258,7 @@ st.markdown('<p class="center fade-in" style="font-size:32px";">🕒 17:45 hs</p
 
 # --- REGALO ---
 CBU = "0720176588000026340436"
-ALIAS = "flora.lucas.boda"
+ALIAS = "FLORA.LUCAS.BODA"
 
 st.markdown(f"""
 <div class="card fade-in" style="text-align:center;">
